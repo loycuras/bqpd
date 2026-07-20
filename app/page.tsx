4c1b599f5fad95fd7c8bef5546afbbd906c966c0
Loyana Jacinto Rodrigues <loycuras05@gmail.com>
2026-07-20 15:16:16 -0300

    Alteração do nome do projeto

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
      return (
        <main style={{ padding: 40, fontFamily: "system-ui" }}>
-         <h1>AvaliaProf</h1>
+         <h1>BQPD - Banco de Questões por Descritor</h1>
          <p>Banco de simulados por descritor — em construção 🚧</p>
        </main>
      );
