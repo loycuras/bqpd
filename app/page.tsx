611cda5c88a8c55e280834a72d4d110f53abb41b
Loyana Jacinto Rodrigues <loycuras05@gmail.com>
2026-07-24 23:57:01 -0300

    Aviso de Premium

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
          <p>Em breve. Dezenas de questões por descritor</p>
          <p>Gere o seu próprio PDF de questões</p>
          <p>Escolha os descritores que quer usar</p>
+         <p>Você também pode baixar aulas prontas na versão premium</p>
        </main>
      );
    }
